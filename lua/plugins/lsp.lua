cdc1fb7733a7ea62e2989e6e66104842f24b0e27
Joshua Brown <jaded423@gmail.com>
2025-10-21 12:02:42 -0500

    updated telescope with ignores to make it's searches faster

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,49 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("mason").setup({
-      ui = {
-        border = "rounded",
-      }
-    })
-    
-    require("mason-lspconfig").setup({
-      ensure_installed = { 
-        "ts_ls",  -- Updated from tsserver
-        "eslint", 
-        "jsonls",
-        "html",
-        "cssls",
-        "pyright",
-      },
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	lazy = false,
+	priority = 50,
+	config = function()
+		require("mason").setup({
+			ui = {
+				border = "rounded",
+			},
+		})
 
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ts_ls",
+				"eslint",
+				"jsonls",
+				"html",
+				"cssls",
+				"pyright",
+			},
+		})
 
-    -- Configure servers
-    local servers = {
-      ts_ls = {},  -- Updated from tsserver
-      eslint = {},
-      jsonls = {},
-      html = {},
-      cssls = {},
-      pyright = {},
-    }
+		-- Get default capabilities from cmp_nvim_lsp
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    for server, config in pairs(servers) do
-      config.capabilities = capabilities
-      lspconfig[server].setup(config)
-    end
-  end,
+		-- Use the new vim.lsp.config API instead of lspconfig
+		local servers = {
+			ts_ls = {},
+			eslint = {},
+			jsonls = {},
+			html = {},
+			cssls = {},
+			pyright = {},
+		}
+
+		for server_name, config in pairs(servers) do
+			-- Merge capabilities into config
+			config.capabilities = capabilities
+
+			-- Use vim.lsp.config instead of require('lspconfig')
+			vim.lsp.config(server_name, config)
+		end
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,12 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
 	},
+	cmd = "Telescope",
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
 		{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
@@ -11,123 +16,41 @@ return {
 		{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
 	},
 	config = function()
-		-- Simple find command with hidden files enabled
-		local find_command = {
-			"rg",
-			"--files",
-			"--follow",
-			"--hidden", -- Include hidden files like .config
-		}
-
-		-- Convert patterns to ripgrep args for live_grep (keeping this working as-is)
-		local function get_ripgrep_args()
-			return {
-				"--follow",
-				-- Same glob patterns as before - this is working well
-				"--glob",
-				"!.*/**",
-				"--glob",
-				"!.*",
-				"--glob",
-				".config/**",
-				"--glob",
-				"!node_modules/**",
-				"--glob",
-				"!Pictures/**",
-				"--glob",
-				"!Photos/**",
-				"--glob",
-				"!Movies/**",
-				"--glob",
-				"!Music/**",
-				"--glob",
-				"!Videos/**",
-				"--glob",
-				"!Downloads/**",
-				"--glob",
-				"!Library/**",
-				-- Media files
-				"--glob",
-				"!*.jpg",
-				"--glob",
-				"!*.jpeg",
-				"--glob",
-				"!*.png",
-				"--glob",
-				"!*.gif",
-				"--glob",
-				"!*.bmp",
-				"--glob",
-				"!*.tiff",
-				"--glob",
-				"!*.webp",
-				"--glob",
-				"!*.svg",
-				"--glob",
-				"!*.ico",
-				"--glob",
-				"!*.mp4",
-				"--glob",
-				"!*.mov",
-				"--glob",
-				"!*.avi",
-				"--glob",
-				"!*.mkv",
-				"--glob",
-				"!*.webm",
-				"--glob",
-				"!*.mp3",
-				"--glob",
-				"!*.wav",
-				"--glob",
-				"!*.flac",
-				"--glob",
-				"!*.aac",
-				"--glob",
-				"!*.pdf",
-				"--glob",
-				"!*.doc",
-				"--glob",
-				"!*.docx",
-				"--glob",
-				"!*.zip",
-				"--glob",
-				"!*.tar",
-				"--glob",
-				"!*.gz",
-			}
-		end
-
 		require("telescope").setup({
 			defaults = {
+				sorting_strategy = "ascending",
+				selection_strategy = "reset",
+				layout_strategy = "horizontal",
 				layout_config = {
 					horizontal = {
+						prompt_position = "top",
 						preview_cutoff = 100,
 						preview_width = 0.6,
 					},
 				},
-				-- Ensure icons are enabled (Nerd Font icons)
-				prompt_prefix = " ",
-				selection_caret = " ",
-				entry_prefix = "  ",
 			},
 			pickers = {
 				find_files = {
-					-- Clean find command - no exclusions, just hidden files enabled
-					find_command = find_command,
-				},
-				live_grep = {
-					-- Keep the working live_grep configuration
-					additional_args = get_ripgrep_args,
-					cwd = vim.fn.expand("~"),
-					search_dirs = {
-						vim.fn.expand("~/.config"),
-						vim.fn.expand("~/Desktop"),
-						vim.fn.expand("~/Documents"),
-						vim.fn.expand("~/projects"),
+					hidden = true,
+					find_command = {
+						"fd",
+						"--type",
+						"f",
+						"--hidden",
+						"--follow",
 					},
 				},
 			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
 		})
+		-- Load the fzf extension
+		require("telescope").load_extension("fzf")
 	end,
 }
