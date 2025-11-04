5d9f27a50bc87e7b44037ac52a58471112a75b93
Joshua Brown <jaded423@gmail.com>
2025-11-04 16:44:23 -0600

    Auto-backup: 2025-11-04 16:44:23

--- lua/plugins/editor/editor.lua
+++ lua/plugins/editor/editor.lua
@@ -59,9 +59,6 @@ return {
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
-				is_always_hidden = function(name, _)
-					return name == ".." or name == ".git"
-				end,
 			},
 			float = {
 				padding = 2,
